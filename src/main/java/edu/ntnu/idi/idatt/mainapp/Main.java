687fc57c87ef9fae9d8a945b0d013b179c5594ff
Christian Petter Remman <cpremman@gmail.com>
2025-03-26 12:23:37 +0100

    feat: Logging - Added creation of local log files upon application start - Fixed logback.xml

--- src/main/java/edu/ntnu/idi/idatt/mainapp/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/mainapp/Main.java
@@ -2,9 +2,12 @@ package edu.ntnu.idi.idatt.mainapp;
 
 import edu.ntnu.idi.idatt.model.boardgames.snakesladders.SnakesAndLadders;
 import edu.ntnu.idi.idatt.view.ConsoleUI;
+import java.io.File;
 
 public class Main {
   public static void main(String[] args) {
+    ensureApplicationDirectoriesExist();
+
     SnakesAndLadders game = new SnakesAndLadders();
     game.initialize();
 
@@ -13,5 +16,19 @@ public class Main {
 
     ConsoleUI ui = new ConsoleUI(game);
     ui.start();
+  }
+
+  private static void ensureApplicationDirectoriesExist() {
+    File logsDir = new File("logs");
+    boolean created = logsDir.mkdirs();
+    if (created) {
+      System.out.println("Logs directory created at: " + logsDir.getAbsolutePath());
+    } else {
+      if (logsDir.exists()) {
+        System.out.println("Logs directory already exists");
+      } else {
+        System.out.println("Failed to create logs directory");
+      }
+    }
   }
 }
