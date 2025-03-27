0b9ad5d59eedc298d54f9ed31093eba932550953
tohiggin <oliver@Thomas-sin-MacBook-Pro.local>
2025-03-27 15:32:45 -0400

    feature: crated testfiles and directories for relavant classes
    - added test for filhandling classes
    - made test csv and jason files to test filreader funct

--- src/test/java/edu/ntnu/idi/idatt/filehandling/FileManagerTest.java
+++ src/test/java/edu/ntnu/idi/idatt/filehandling/FileManagerTest.java
@@ -3,8 +3,12 @@ package edu.ntnu.idi.idatt.filehandling;
 import edu.ntnu.idi.idatt.exceptions.FileReadException;
 import edu.ntnu.idi.idatt.exceptions.FileWriteException;
 import edu.ntnu.idi.idatt.exceptions.CsvFormatException;
+import edu.ntnu.idi.idatt.exceptions.JsonParsingException;
 import edu.ntnu.idi.idatt.model.boardgames.snakesladders.Board;
 import edu.ntnu.idi.idatt.model.boardgames.snakesladders.SnakesAndLadders;
+import edu.ntnu.idi.idatt.model.boardgames.snakesladders.tile.LadderTile;
+import edu.ntnu.idi.idatt.model.boardgames.snakesladders.tile.SnakeTile;
+import edu.ntnu.idi.idatt.model.boardgames.snakesladders.tile.Tile;
 import org.junit.jupiter.api.*;
 
 import java.io.File;
@@ -74,5 +78,28 @@ class FileManagerTest {
     assertEquals(2, loaded, "Should load 2 players");
     assertEquals("Kari", loadedGame.getPlayers().get(0).getName());
     assertEquals("Per", loadedGame.getPlayers().get(1).getName());
+  }
+
+  @Test
+  void testBoardIsSavedAndLoadedCorrectly() throws FileWriteException, FileReadException, JsonParsingException {
+    // Lag et brett og sett inn en stige og en slange
+    Board board = new Board();
+    board.setTile(5, new LadderTile(5, 15));
+    board.setTile(10, new SnakeTile(10, 3));
+
+    FileManager.saveDefaultBoard(board);
+
+    Board loaded = new BoardJsonHandler().loadFromFile(FileManager.DEFAULT_BOARD_FILE);
+
+    assertNotNull(loaded, "Loaded board should not be null");
+
+    Tile ladder = loaded.getTile(5);
+    Tile snake = loaded.getTile(10);
+
+    assertTrue(ladder.hasSnakeOrLadder(), "Tile 5 should be a ladder");
+    assertEquals(15, ladder.getDestination(), "Ladder destination should be 15");
+
+    assertTrue(snake.hasSnakeOrLadder(), "Tile 10 should be a snake");
+    assertEquals(3, snake.getDestination(), "Snake destination should be 3");
   }
 }
--- src/test/java/edu/ntnu/idi/idatt/model/boardgames/snakesladders/SnakesAndLaddersTest.java
+++ src/test/java/edu/ntnu/idi/idatt/model/boardgames/snakesladders/SnakesAndLaddersTest.java
@@ -1,4 +1,40 @@
 package edu.ntnu.idi.idatt.model.boardgames.snakesladders;
 
-public class SnakesAndLaddersTest {
+import edu.ntnu.idi.idatt.model.boardgames.snakesladders.tile.Tile;
+import edu.ntnu.idi.idatt.model.common.player.Player;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SnakesAndLaddersTest {
+
+  SnakesAndLadders game;
+
+  @BeforeEach
+  void setUp() {
+    game = new SnakesAndLadders();
+    game.addPlayer("A");
+    game.addPlayer("B");
+    game.initialize(); // init board & dice
+  }
+
+  @Test
+  void testAddPlayers() {
+    assertEquals(2, game.getPlayers().size());
+    assertEquals("A", game.getPlayers().get(0).getName());
+  }
+
+  @Test
+  void testGetCurrentPlayer() {
+    Player current = game.getCurrentPlayer();
+    assertEquals("A", current.getName());
+  }
+
+  @Test
+  void testMakeMoveChangesPlayerTurn() {
+    Player first = game.getCurrentPlayer();
+    game.makeMove(first);
+    Player second = game.getCurrentPlayer();
+    assertNotEquals(first, second, "Player turn should change after move");
+  }
 }
