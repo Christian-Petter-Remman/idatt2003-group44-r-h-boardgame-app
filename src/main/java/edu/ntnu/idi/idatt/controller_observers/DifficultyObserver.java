adc5a83f84c5116bb5cb9b2453991c97bd7a2ce6
Christian Petter Remman <cpremman@gmail.com>
2025-04-04 18:03:12 +0200

    feat: DifficultyObserver - Renamed observer directory to model_observers - Observer activates and shows template difficulty choices

---
+++ src/main/java/edu/ntnu/idi/idatt/controller_observers/DifficultyObserver.java
@@ -0,0 +1,5 @@
+package edu.ntnu.idi.idatt.controller_observers;
+
+public interface DifficultyObserver {
+  void onDifficultyChanged(String difficulty);
+}
--- src/main/java/edu/ntnu/idi/idatt/model/model_observers/game/GameObserver.java
+++ src/main/java/edu/ntnu/idi/idatt/model/model_observers/game/GameObserver.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idi.idatt.model.observers.game;
+package edu.ntnu.idi.idatt.model.model_observers.game;
 
 public interface GameObserver {
   void onGameStart();
