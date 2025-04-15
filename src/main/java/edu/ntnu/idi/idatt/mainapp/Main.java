0fe5d073aab52cbb800b5409bad3d43c0ce2f1fc
Christian Petter Remman <cpremman@gmail.com>
2025-04-15 21:31:30 +0200

    Refactor: Main - Removed MainGUI - Refactored Main

--- src/main/java/edu/ntnu/idi/idatt/mainapp/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/mainapp/Main.java
@@ -1,15 +1,26 @@
 package edu.ntnu.idi.idatt.mainapp;
 
+import static edu.ntnu.idi.idatt.util.AlertUtil.showAlert;
+
+import edu.ntnu.idi.idatt.filehandling.FileManager;
 import edu.ntnu.idi.idatt.view.common.IntroScreenView;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main extends Application {
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
   @Override
   public void start(Stage primaryStage) {
+    try {
+      FileManager.ensureApplicationDirectoriesExist();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
     try {
       IntroScreenView intro = new IntroScreenView(primaryStage);
       intro.prepareScene();
@@ -23,20 +34,9 @@ public class Main extends Application {
       Platform.runLater(() -> primaryStage.setFullScreen(true));
 
     } catch (Exception e) {
-      showStartupError("Startup Error", e.getMessage());
-      e.printStackTrace();
+      logger.error("Error during startup: {}", e.getMessage());
+      showAlert("Startup Error", "Could not start the game");
     }
-  }
-
-  private void showStartupError(String title, String message) {
-    javafx.scene.control.Alert alert = new javafx.scene.control.Alert(
-            javafx.scene.control.Alert.AlertType.ERROR,
-            message,
-            javafx.scene.control.ButtonType.OK
-    );
-    alert.setTitle(title);
-    alert.setHeaderText(null);
-    alert.showAndWait();
   }
 
   public static void main(String[] args) {
