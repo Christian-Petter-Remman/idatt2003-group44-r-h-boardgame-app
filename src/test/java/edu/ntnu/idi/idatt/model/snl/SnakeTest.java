4b7cbdcdc6c5fac358815125c91bf426f03f88f7
Christian Petter Remman <cpremman@gmail.com>
2025-05-21 23:46:42 +0200

    test: SNL Game Model - Implemented even more tests for SNL Model

---
+++ src/test/java/edu/ntnu/idi/idatt/model/snl/SnakeTest.java
@@ -0,0 +1,37 @@
+package edu.ntnu.idi.idatt.model.snl;
+
+import edu.ntnu.idi.idatt.model.common.AbstractBoard;
+import edu.ntnu.idi.idatt.model.common.Player;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SnakeTest {
+
+  static class DummyPlayer extends Player {
+    public DummyPlayer(String name, String character) { super(name, character); }
+    @Override public boolean hasWon() { return false; }
+    @Override public int getStartPosition() { return 1; }
+  }
+
+  static class DummyBoard extends AbstractBoard {
+    public DummyBoard(int size) { super(size); }
+    @Override public int getSize() { return 100; }
+  }
+
+  @Test
+  void testConstructorAndGetters() {
+    Snake snake = new Snake(16, 6);
+    assertEquals(16, snake.getStart());
+    assertEquals(6, snake.getEnd());
+  }
+
+  @Test
+  void testOnLandMovesPlayerToEnd() {
+    Snake snake = new Snake(20, 4);
+    DummyPlayer player = new DummyPlayer("Nils", "icon");
+    DummyBoard board = new DummyBoard(100);
+    player.setPosition(20);
+    snake.onLand(player, board);
+    assertEquals(4, player.getPosition());
+  }
+}
