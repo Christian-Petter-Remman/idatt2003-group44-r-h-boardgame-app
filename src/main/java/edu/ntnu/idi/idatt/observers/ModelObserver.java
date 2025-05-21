2909ee454170c2462d45ef39f61fd5ab96d68b75
Christian Petter Remman <cpremman@gmail.com>
2025-05-22 01:10:18 +0200

    refactor: Redundant Modules - Removed redundant classes and interfaces

--- src/main/java/edu/ntnu/idi/idatt/observers/ModelObserver.java
+++
@@ -1,5 +0,0 @@
-package edu.ntnu.idi.idatt.observers;
-
-public interface ModelObserver {
-  void update(String eventType, Object data);
-}
--- src/main/java/edu/ntnu/idi/idatt/view/snl/SNLRuleSelectionViewObserver.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.idi.idatt.view.snl;
-
-import edu.ntnu.idi.idatt.model.snl.SNLRuleSelectionModel;
-
-public interface SNLRuleSelectionViewObserver {
-  void onRuleSelectionChanged(SNLRuleSelectionModel model);
-}
